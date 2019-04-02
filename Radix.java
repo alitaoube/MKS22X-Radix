012f38567327efeb93791afdcca2c9d422c4488f
Ali Taoube <ataoube00@stuy.edu>
2019-04-02 10:11:47 -0400

    Converted findBiggest to O(N)

--- Radix.java
+++ Radix.java
@@ -3,22 +3,23 @@ public class Radix{
     int[] data = {10, 5, 10000, 1000, 20, 5, 1000000, 100000000}; // should return 7
     System.out.print(findBiggest(data));
   }
-  // public static void radixsort(int[]data){
-  //   MyLinkedList[] buckets = new MyLinkedList[20];
-  //
-  //
-  // }
+  public static void radixsort(int[]data){
+    int highest = findBiggest(data);
+    MyLinkedList[] buckets = new MyLinkedList[20];
+
+    for (int x = 0; x < highest; x++){
+
+    }
+
+  }
   private static int findBiggest(int[] data){
     int highest = 0;
+
     for (int x = 0; x < data.length; x++){
-      int num = data[x];
-      int track = 0;
-      for (int y = 0; num != 0; y++){
-        track++;
-        num /= 10;
-      }
-      if (track >= highest) highest = track;
+      if (data[x] > highest) highest = data[x];
     }
-    return highest;
+
+    if (highest < 0) return (highest + "").length() - 1;
+    return (highest + "").length();
   }
 }
