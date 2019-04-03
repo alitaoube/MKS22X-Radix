517c8d421b17d93fa5fe4f92c103ea6498df6b4f
Ali Taoube <ataoube00@stuy.edu>
2019-04-03 09:37:50 -0400

    Finished radix sort function, need to test

--- Radix.java
+++ Radix.java
@@ -1,17 +1,20 @@
 public class Radix{
-  public static void main(String[] args) {
-    int[] data = {10, 5, 10000, 1000, 20, 5, 1000000, 100000000}; // should return 7
-    System.out.print(findBiggest(data));
-  }
+
+  
   public static void radixsort(int[]data){
     int highest = findBiggest(data);
     MyLinkedList[] buckets = new MyLinkedList[20];
 
     for (int x = 0; x < highest; x++){
+      buckets[data[x] % (int)Math.pow(10, x) / 10 + 9].add(data[x]); // add whole number not just digit
 
+      for (int i = 0; i < buckets.length - 1; i++) {
+        buckets[i].extend(buckets[i + 1]);
+      }
     }
-
   }
+
+
   private static int findBiggest(int[] data){
     int highest = 0;
 
